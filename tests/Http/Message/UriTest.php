f7ef9b7630ad753d6bd2e61fcf0cf2891abb0dae
Jason Lamb <jason@ridango.com>
2018-04-05 18:38:12 +0300

    New tests for Uri + UriTest
    * Test to ensure withScheme returns new instance if different scheme
    * Test to ensure withScheme returns same instance if scheme is identical
    * Test to ensure withScheme only accepts strings
    * Test to ensure HTTP + HTTPS are supported
    * Test for exception if invalid data-type is passed
    * Test to ensure exception if invalid scheme is used
    * Test to ensure scheme is converted to lower-case
    * Test to ensure removal of ://

--- tests/Http/Message/UriTest.php
+++ tests/Http/Message/UriTest.php
@@ -3,7 +3,9 @@
 namespace Tests\Http\Message;
 
 use BlcksheepIO\Http\Message\Uri;
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
+use stdClass;
 
 /**
  * Class UriTest
@@ -27,27 +29,75 @@ class UriTest extends TestCase
         //self::assertEquals('http://user:pass@local.example.com:3001/foo?bar=baz#quz', (string) $new);
     }
 
+    public function testReturnsSameInstanceIfWithSchemeIsIdentical()
+    {
+        $uri = new Uri();
+        $new = $uri->withScheme('http');
+        self::assertSame($new, $new->withScheme('http'));
+    }
+
+    public function testWithSchemeAcceptsEmptyString()
+    {
+        $uri = new Uri();
+        $uri->withScheme('');
+        self::assertEmpty($uri->getScheme());
+    }
+
     /**
      * @return array
      */
-    public function mixedCaseSchemeDataProvider()
+    public function validSchemeDataProvider()
     {
         return [
-            ['http'],
-            ['Http'],
-            ['HTTP'],
+            ['http', 'http'],
+            ['Http', 'http'],
+            ['HTTP', 'http'],
+            ['https', 'https'],
+            ['HttpS', 'https'],
+            ['HTTPS', 'https'],
+            ['http://', 'http'],
+            ['Http://', 'http'],
+            ['HTTP://', 'http'],
+            ['https://', 'https'],
+            ['HttpS://', 'https'],
+            ['HTTPS://', 'https'],
         ];
     }
 
     /**
      * @param $scheme
-     * @dataProvider mixedCaseSchemeDataProvider
+     * @param $expected
+     * @dataProvider validSchemeDataProvider
      */
-    public function testWithSchemeConvertsSchemeToLowerCase($scheme)
+    public function testWithSchemeConvertsSchemeToLowerCase($scheme, $expected)
     {
         $uri = new Uri();
         $new = $uri->withScheme($scheme);
-        self::assertSame(strtolower($scheme), $new->getScheme());
+        self::assertSame($expected, $new->getScheme());
+    }
+
+    /**
+     * @param $scheme
+     * @param $expected
+     * @dataProvider validSchemeDataProvider
+     */
+    public function testWithSchemeSupportsHttpAndHttpsAndDoesNotRaiseException($scheme, $expected)
+    {
+        $uri = new Uri();
+        $new = $uri->withScheme($scheme);
+        self::assertEquals($expected, $new->getScheme());
+    }
+
+    /**
+     * @param $scheme
+     * @param $expected
+     * @dataProvider validSchemeDataProvider
+     */
+    public function testWithSchemeRemovesExtraCharacters($scheme, $expected)
+    {
+        $uri = new Uri();
+        $new = $uri->withScheme($scheme);
+        self::assertEquals($expected, $new->getScheme());
     }
 
     public function testGetSchemeReturnsAnEmptyStringIfSchemeIsNotPresent()
@@ -55,5 +105,41 @@ class UriTest extends TestCase
         $uri = new Uri();
         self::assertInternalType('string', $uri->getScheme());
         self::assertEmpty($uri->getScheme());
+    }
+
+    public function testWithSchemeThrowsExceptionIfInvalidSchemeIsRequested()
+    {
+        self::expectException(InvalidArgumentException::class);
+        self::expectExceptionMessage('Unsupported scheme requested "ftp"; must be empty or in the set (http, https)');
+
+        $uri = new Uri();
+        $uri->withScheme('ftp');
+    }
+
+    /**
+     * @return array
+     */
+    public function invalidPHPDataTypesForSchemeDataProvider()
+    {
+        return [
+            [123],
+            [true],
+            [new stdClass()],
+            [10.55],
+            [['foo', 'bar']],
+        ];
+    }
+
+    /**
+     * @param $scheme
+     * @dataProvider invalidPHPDataTypesForSchemeDataProvider
+     */
+    public function testWithSchemeThrowsExceptionIfInvalidDataTypeIsUsed($scheme)
+    {
+        self::expectException(InvalidArgumentException::class);
+        self::expectExceptionMessage('BlcksheepIO\Http\Message\Uri::withScheme expects a string argument; received ' . ((is_object($scheme)) ? get_class($scheme) : gettype($scheme)));
+
+        $uri = new Uri();
+        $uri->withScheme($scheme);
     }
 }
