07b71a64bc2abf5f3d6c115322880a5da4c4595b
Jason Lamb <jason@ridango.com>
2018-04-05 17:44:02 +0300

    Various misc changes, getting environment up and running better.

--- tests/Http/Message/UriTest.php
+++ tests/Http/Message/UriTest.php
@@ -16,5 +16,44 @@ class UriTest extends TestCase
         $uri = new Uri();
         $new = $uri->withScheme('http');
         self::assertNotSame($uri, $new);
+        self::assertEquals('http', $new->getScheme());
+
+        /*
+         * This SHOULD be enabled once I have the FULL code
+         * developed.
+         *
+         * TODO: Enable this
+         */
+        //self::assertEquals('http://user:pass@local.example.com:3001/foo?bar=baz#quz', (string) $new);
+    }
+
+    /**
+     * @return array
+     */
+    public function mixedCaseSchemeDataProvider()
+    {
+        return [
+            ['http'],
+            ['Http'],
+            ['HTTP'],
+        ];
+    }
+
+    /**
+     * @param $scheme
+     * @dataProvider mixedCaseSchemeDataProvider
+     */
+    public function testWithSchemeConvertsSchemeToLowerCase($scheme)
+    {
+        $uri = new Uri();
+        $new = $uri->withScheme($scheme);
+        self::assertSame(strtolower($scheme), $new->getScheme());
+    }
+
+    public function testGetSchemeReturnsAnEmptyStringIfSchemeIsNotPresent()
+    {
+        $uri = new Uri();
+        self::assertInternalType('string', $uri->getScheme());
+        self::assertEmpty($uri->getScheme());
     }
 }
